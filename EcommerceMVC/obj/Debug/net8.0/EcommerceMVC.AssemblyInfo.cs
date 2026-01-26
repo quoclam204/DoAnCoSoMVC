c88e2cd7dc828de186997cc5f662ae800dc56861
quoclam204 <nguyenlequoclam@gmail.com>
2026-01-27 01:16:49 +0700

    Update

--- EcommerceMVC/obj/Debug/net8.0/EcommerceMVC.AssemblyInfo.cs
+++ EcommerceMVC/obj/Debug/net8.0/EcommerceMVC.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72edeae59cf1a9bd9ca8686ea5fff3a3c91e58d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
